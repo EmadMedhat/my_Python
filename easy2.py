7a135df3c204403a27d0657c4a0ffd8d7c879468
Emad Manqryous <emadmedhat40@gmail.com>
2024-07-31 16:28:59 +0300

    "Write a Python program to find all the unique words and count the frequency of occurrence from a given list of strings."

---
+++ easy2.py
@@ -0,0 +1,16 @@
+mylist=['name1','name0','name2','name5','name5','name1','name2','name3','name4','name5']
+i=0 ;
+unique=mylist[:] #copy a list
+while i<=len(unique)-1:
+    first=unique[i]
+    c=mylist.count(first)
+    if c==1:   
+        print ('this word is unique: ',first)  
+        unique.remove(first)         
+    elif c>=2:   
+        print ('this word frequency of occurrence is: ',str(c),' and the word is: ', first)
+        for x in range(c):
+            unique.remove(first)
+    i=0    
+           
+       
